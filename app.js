ece7cc43c8043c0fe10252e22e557e30b2467be5
tvrtko <tvrtko.mrkonjic@hr.ibm.com>
2017-05-23 16:41:36 +0200

    added favicon

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
+
+var app = express();
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello Worlda!')
-})
+  res.send('Hello Worlda!');
+});
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port)
-})
+  console.log('Example app listening on port ' + port);
+});
